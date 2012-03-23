3f0ead5b165c69f49ac7ed6ed19bfd761fae9b3f
MendelGusmao <mendelsongusmao@gmail.com>
2012-03-22 23:41:04 -0300

    Improving simplecache and rss

--- rss.rb
+++ rss.rb
@@ -6,7 +6,7 @@ require_relative './simplecache'
 # $dll(cgi,1,rss.rb,rss#<feed url>;<item index>)
 def rss url, index = 0
     parser = Syndication::RSS::Parser.new
-    feed = parser.parse(cache(url, 900))
+    feed = parser.parse(cache(url, 900)[0])
     feed.items[index.to_i].title unless feed.items[index.to_i].nil?
 end
 
--- simplecache.rb
+++ simplecache.rb
@@ -1,9 +1,20 @@
 require 'digest/sha1'
+require 'fileutils'
 
-def cache url, timeout = 60
+def cache url, timeout = 60, forced_content = nil
     content = ""
+    missed = false
     
     cache_file = [ "cache/", Digest::SHA1.hexdigest(url), ".txt" ].join("")
+
+    unless forced_content.nil?
+      open(cache_file, "wb") { |file| file.write(forced_content) }
+      return forced_content
+    end
+
+    if !File::exists?(url)
+      url = cache_file 
+    end
 
     if !File::exists?(cache_file)
         FileUtils::touch([cache_file]) 
@@ -11,11 +22,13 @@ def cache url, timeout = 60
     end
     
     if Time.now - File.new(cache_file).mtime > timeout
-        open(url) { |file| content = file.read }
-        open(cache_file, "w") { |file| file.write(content) }
+        open(url, "rb") { |file| content = file.read }
+        open(cache_file, "wb") { |file| file.write(content) }
+        missed = true
     else
-        open(cache_file) { |file| content = file.read }
+        open(cache_file, "rb") { |file| content = file.read }
+        missed = false
     end
    
-    content
+    [content, missed]
 end
