df85eb9de21d0010f2bcdaf42a12601f4cc35ca6
MendelGusmao <mendelsongusmao@gmail.com>
2012-03-24 01:03:28 -0300

    Improved RSS caching

--- rss.rb
+++ rss.rb
@@ -3,11 +3,32 @@ require 'fileutils'
 require 'syndication/rss'
 require_relative './simplecache'
 
+$timeout = 300
+
 # $dll(cgi,1,rss.rb,rss#<feed url>;<item index>)
 def rss url, index = 0
-    parser = Syndication::RSS::Parser.new
-    feed = parser.parse(cache(url, 900)[0])
-    feed.items[index.to_i].title unless feed.items[index.to_i].nil?
+
+  cached_items = cache("file://cache/rss/" + url, 60).first
+
+  begin
+    cached_items = Marshal.load(cached_items)
+  rescue
+    cached_items = []
+  end
+
+  parser = Syndication::RSS::Parser.new
+
+  items = parser.parse(cache(url, $timeout).first).items.map { |item|
+    [item.pubdate, item.title]
+  }
+
+  items += cached_items
+  items = items.compact.uniq.sort_by{ |i| i.first }.reverse.slice(0..10)
+
+  cache("file://cache/rss/" + url, 0, true, Marshal.dump(items))
+  ""
+  items[index.to_i].last unless items.nil? or items[index.to_i].nil?
+
 end
 
 main()
