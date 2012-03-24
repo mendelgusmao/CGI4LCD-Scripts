5e77210c7958566b6cf357c76e4642e4a91ebdad
MendelGusmao <mendelsongusmao@gmail.com>
2012-03-24 01:07:16 -0300

    General improvements and tasteometer rank to show the Nth friend with more compatible music taste

--- lastfm.rb
+++ lastfm.rb
@@ -1,14 +1,13 @@
 require "nokogiri"
 require 'open-uri'
-require 'fileutils'
 require_relative './simplecache'
 
 $api_key = "YOUR API KEY HERE"
 $user = "YOUR LASTFM USERNAME HERE"
 $timeout = 900
 
-def _request user, method, limit
-  url = "http://ws.audioscrobbler.com/2.0/?method=%s&user=%s&limit=%s&api_key=%s" % [method, user, limit, $api_key]
+def _request user, method, limit, append = ""
+  url = "http://ws.audioscrobbler.com/2.0/?method=%s&user=%s&limit=%s&api_key=%s&%s" % [method, user, limit, $api_key, append]
   data, missed = cache(url, $timeout)
   [Nokogiri::XML(data), missed]
 end
@@ -16,7 +15,7 @@ end
 # $dll(cgi,1,lastfm.rb,recent#<index>)
 def recent index
 
-  cached_tracks, missed = cache("cache/lastfm", 60)
+  cached_tracks, missed = cache("file://cache/lastfm" + index, 60, true)
 
   if missed
 
@@ -37,10 +36,9 @@ def recent index
         artist = track.xpath("artist").text
         title = track.xpath("name").text
         album = track.xpath("album").text
-        formatted = "%s - %s (%s)" % [artist, title, album]
-        played = track.xpath("date").first.attributes["uts"].value unless track.xpath("date").empty?
+        played = track.xpath("date").first.attributes["uts"].value.to_i unless track.xpath("date").empty?
         
-        recent_tracks << [friend_name, formatted, played.to_i]
+        recent_tracks << [friend_name, artist, title, album, played]
 
       end
 
@@ -48,12 +46,45 @@ def recent index
 
     end
 
-    cache("cache/lastfm", 0, Marshal.dump(recent_tracks))
+    recent_tracks.sort_by! { |a| a.last.to_i }.reverse!
+    cache("file://cache/lastfm" + index, 0, true, Marshal.dump(recent_tracks[0..9]))
   else
     recent_tracks = Marshal.load(cached_tracks)
   end
 
-  "%s: %s" % recent_tracks.sort_by! { |a| a[2] }.reverse![index.to_i]
+  "[%s] %s - %s" % recent_tracks[index.to_i]
+
+end
+
+def tasteometer index
+
+  cached_tracks, missed = cache("file://cache/lastfm/taste" + index, 120, true)
+
+  if missed
+
+    friends = _request($user, "user.getfriends", 100)[0].xpath("//friends/user")
+    comparisons = []
+
+    friends.each do |friend|
+      friend_name = friend.xpath("name").text
+      
+      comparison, missed = _request(friend_name, "tasteometer.compare", 3, "type1=user&type2=user&value1=%s&value2=%s" % [$user, friend_name])
+      bands = comparison.xpath("//comparison/result/artists/artist").map { |e| e.xpath("name") }.join ", "
+      comparison = comparison.xpath("//comparison/result/score").text
+      
+      comparisons << [ friend_name, comparison.to_f * 100, bands ]
+
+      sleep 1 if missed
+
+    end
+
+    comparisons.sort_by! { |a| a[1] }.reverse!
+    cache("file://cache/lastfm/taste" + index, 0, true, Marshal.dump(comparisons[0..9]))
+  else
+    comparisons = Marshal.load(cached_tracks)
+  end
+
+  "[%s] %02.1f%% (%s)" % comparisons[index.to_i]
 
 end
 
