af467a7c9e0dde5e4cf42f6a71a78ad8ba1ba5f6
MendelGusmao <mendelsongusmao@gmail.com>
2012-03-31 14:16:52 -0300

    Improving simplecache and rss

--- rss.rb
+++ rss.rb
@@ -8,26 +8,20 @@ $timeout = 300
 # $dll(cgi,1,rss.rb,rss#<feed url>;<item index>)
 def rss url, index = 0
 
-  cached_items = cache("file://cache/rss/" + url, 60).first
-
-  begin
-    cached_items = Marshal.load(cached_items)
-  rescue
-    cached_items = []
-  end
-
   parser = Syndication::RSS::Parser.new
+  feed = parser.parse(Simplecache::cache(url, $timeout).first)
 
-  items = parser.parse(cache(url, $timeout).first).items.map { |item|
-    [item.pubdate, item.title]
-  }
-
-  items += cached_items
-  items = items.compact.uniq.sort_by{ |i| i.first }.reverse.slice(0..10)
-
-  cache("file://cache/rss/" + url, 0, true, Marshal.dump(items))
-  ""
-  items[index.to_i].last unless items.nil? or items[index.to_i].nil?
+  items = feed.items.nil? ? [] : feed.items.map { |item| [item.pubdate, item.title] } 
+  
+  items = Simplecache::store("file://cache/rss/" + url, items) do |content|
+    content.compact.uniq.sort_by{ |i| i.first }.reverse.slice(0..10)
+  end
+  
+  unless items.nil? or items[index.to_i].nil?
+    items[index.to_i].last
+  else
+    ""
+  end
 
 end
 
--- simplecache.rb
+++ simplecache.rb
@@ -1,36 +1,64 @@
 require 'digest/sha1'
 require 'fileutils'
 
-def cache url, timeout = 60, binary = false, forced_content = nil
-    content = ""
-    missed = false
-    
-    binary = binary ? "b" : ""
-    
-    cache_file = [ "cache/", Digest::SHA1.hexdigest(url), ".txt" ].join("")
+class Simplecache
 
-    unless forced_content.nil?
-      open(cache_file, "w" + binary) { |file| file.write(forced_content) }
-      return forced_content
-    end
-
-    if url[/^file:/] and !File::exists?(url)
-      url = cache_file 
-    end
-
-    if !File::exists?(cache_file)
-        FileUtils::touch([cache_file]) 
-        timeout = 0
-    end
-    
-    if Time.now - File.new(cache_file).mtime > timeout
-        open(url, "r" + binary) { |file| content = file.read }
-        open(cache_file, "w" + binary) { |file| file.write(content) }
-        missed = true
-    else
-        open(cache_file, "rb" + binary) { |file| content = file.read }
+    def self.cache url, timeout = 60, binary = false, forced_content = nil
+        content = ""
         missed = false
+        
+        binary = binary ? "b" : ""
+        
+        cache_file = [ "cache/", Digest::SHA1.hexdigest(url), ".txt" ].join("")
+
+        unless forced_content.nil?
+          open(cache_file, "w" + binary) { |file| file.write(forced_content) }
+          return forced_content
+        end
+
+        if url[/^file:/] and !File::exists?(url)
+          url = cache_file 
+        end
+
+        if !File::exists?(cache_file)
+            FileUtils::touch([cache_file]) 
+            timeout = 0
+        end
+        
+        if Time.now - File.new(cache_file).mtime > timeout
+            open(url, "r" + binary) { |file| content = file.read }
+            open(cache_file, "w" + binary) { |file| file.write(content) }
+            missed = true
+        else
+            open(cache_file, "r" + binary) { |file| content = file.read }
+            missed = false
+        end
+       
+        [content, missed, cache_file]
     end
-   
-    [content, missed]
-end
+    
+    def self.store url, to_append = nil, &block
+
+        cache_file = [ "cache/", Digest::SHA1.hexdigest(url), ".txt" ].join("")
+        
+        if !File::exists?(cache_file)
+            FileUtils::touch([cache_file]) 
+        end        
+        
+        content = open(cache_file, "rb") { |file| file.read }
+        
+        begin
+            content = Marshal.load(content)
+        rescue
+            content = []
+        end
+        
+        content += to_append
+        content = yield(content) if block_given?
+        
+        open(cache_file, "wb") { |file| file.write(Marshal.dump(content)) }
+        
+        content
+    end
+
+end    
