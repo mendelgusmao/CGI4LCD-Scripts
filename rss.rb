484fb9cccaabd577519998fae923a158ec55eaef
MendelGusmao <mendelsongusmao@gmail.com>
2012-04-02 00:36:35 -0300

    Bug fixes

--- rss.rb
+++ rss.rb
@@ -13,31 +13,30 @@ def rss url, type = "t", itemnum = 0, maxfreq = $timeout
   feed = parser.parse(Simplecache::cache(url, maxfreq).first)
 
   items = feed.items.nil? ? [] : feed.items.map do |item| 
-    body = case type
-      when "t" then item.title
-      when "d" then item.description
-      when "b" then item.title + ":" + item.description
-    end
+    body = { "t" => item.title, 
+             "d" => item.description, 
+             "b" => "%s:%s" % [item.title, item.description] }[type]
+
     [ item.pubdate, body ]
   end
-  
-  items = Simplecache::store(url, items) do |content, to_append|
+
+  items = Simplecache::store(url + type, items) do |content, to_append|
     content += to_append
     content.compact.uniq.sort_by{ |i| i.first }.reverse.slice(0..10)
   end
-  
+
+  response = ""
+
   if itemnum == 0
-    response = items.join(" | ")
+    response = items.map{ |i| i.last }.join(" | ")
   else
-    unless items.nil? or items[itemnum].nil?
-      response = items[itemnum].last
-    else
-      response = ""
+    unless items.nil? or items[itemnum - 1].nil?
+      response = items[itemnum - 1].last
     end
   end
 
-  response.gsub!(/<\/?[^>]*>/, "").gsub!(/&[a-z]+;/, " ")
-
+  response.gsub(/<\/?[^>]*>/, "")
+  
 end
 
 main()
