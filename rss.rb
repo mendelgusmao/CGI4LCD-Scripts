2f06c00b9c9fb50849e3bcbf614f7322482142c3
MendelGusmao <mendelsongusmao@gmail.com>
2012-04-02 00:01:34 -0300

    Making it compatible with LCD Smartie's $Rss function

--- rss.rb
+++ rss.rb
@@ -1,26 +1,42 @@
 require 'syndication/rss'
 require_relative './simplecache'
 
-$timeout = 300
+$timeout = 300 # seconds
 
-# $dll(cgi,1,rss.rb,rss#<feed url>;<item index>)
-def rss url, index = 0
+# $dll(cgi,1,rss.rb,rss#<feed url>;<type>;<item index>;<maxfreq>)
+def rss url, type = "t", itemnum = 0, maxfreq = $timeout
+
+  type = "t" unless type[/[tdb]/]
+  itemnum = itemnum.to_i
 
   parser = Syndication::RSS::Parser.new
-  feed = parser.parse(Simplecache::cache(url, $timeout).first)
+  feed = parser.parse(Simplecache::cache(url, maxfreq).first)
 
-  items = feed.items.nil? ? [] : feed.items.map { |item| [item.pubdate, item.title] } 
+  items = feed.items.nil? ? [] : feed.items.map do |item| 
+    body = case type
+      when "t" then item.title
+      when "d" then item.description
+      when "b" then item.title + ":" + item.description
+    end
+    [ item.pubdate, body ]
+  end
   
   items = Simplecache::store(url, items) do |content, to_append|
     content += to_append
     content.compact.uniq.sort_by{ |i| i.first }.reverse.slice(0..10)
   end
   
-  unless items.nil? or items[index.to_i].nil?
-    items[index.to_i].last
+  if itemnum == 0
+    response = items.join(" | ")
   else
-    ""
+    unless items.nil? or items[itemnum].nil?
+      response = items[itemnum].last
+    else
+      ""
+    end
   end
+
+  response.gsub!(/<\/?[^>]*>/, "").gsub!(/&[a-z]+;/, " ")
 
 end
 
